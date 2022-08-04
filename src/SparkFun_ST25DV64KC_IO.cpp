7e9b0285b25249acdd94ddf492233506fdc42067
PaulZC <pc235603@gmail.com>
2022-08-04 11:52:37 +0100

    Make read and write more robust:
    Read and write functions will now retry multiple times if the IC is still busy completing the previous write.
    Multiple byte reads and writes are divided up into chunks if required.

--- src/SparkFun_ST25DV64KC_IO.cpp
+++ src/SparkFun_ST25DV64KC_IO.cpp
@@ -33,91 +33,200 @@ bool SFE_ST2525DV64KC_IO::isConnected()
 
 bool SFE_ST2525DV64KC_IO::writeMultipleBytes(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, uint8_t *const buffer, uint16_t const packetLength)
 {
-  _i2cPort->beginTransmission(static_cast<int>(address));
+  // Split long writes up into multiple chunks
+  uint16_t bytesWritten = 0;
 
-  uint8_t temp = static_cast<uint16_t>(registerAddress) >> 8;
-  _i2cPort->write(temp);
-  temp = registerAddress & 0xff;
-  _i2cPort->write(temp);
+  // If the IC is busy - e.g. completing a previous write - the I2C transmission is NACK'd and fails.
+  // For each chunk: try up to maxRetries times, waiting retryDelay ms between tries.
+  bool result = true;
+  uint8_t maxTries = maxRetries;
 
-  for (uint16_t i = 0; i < packetLength; i++)
-    _i2cPort->write(buffer[i]);
+  while ((bytesWritten < packetLength) && (result))
+  {
+    uint8_t bytesToWrite; // Write the data in chunks of readWriteChunkSize max
+    if ((packetLength - bytesWritten) > ((uint16_t)readWriteChunkSize))
+      bytesToWrite = readWriteChunkSize;
+    else
+      bytesToWrite = packetLength - bytesWritten;
 
-  bool success = (_i2cPort->endTransmission() == 0);
+    _i2cPort->beginTransmission(static_cast<int>(address));
 
-  // IC needs approx. 5ms to complete its writing phase.
-  delay(10);
+    uint8_t temp = static_cast<uint16_t>(registerAddress + bytesWritten) >> 8;
+    _i2cPort->write(temp);
+    temp = (registerAddress + bytesWritten) & 0xff;
+    _i2cPort->write(temp);
+
+    for (uint16_t i = 0; i < bytesToWrite; i++)
+      _i2cPort->write(buffer[i + bytesWritten]);
+
+    bool success = (_i2cPort->endTransmission() == 0);
+
+    if (success)
+    {
+      bytesWritten += bytesToWrite;
+      maxTries = maxRetries;
+    }
+    else
+    {
+      maxTries--;
+      if (maxTries == 0)
+        result = false;
+      else
+        delay(retryDelay);
+    }
+  }
+
+  return result;
+}
+
+bool SFE_ST2525DV64KC_IO::readMultipleBytes(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, uint8_t *const buffer, const uint16_t packetLength)
+{
+  bool success = true; // Return true if packetLength is zero
+
+  // Split long reads up into multiple chunks
+  uint16_t bytesRead = 0;
+
+  // If the IC is busy - e.g. completing a previous write - the I2C transmission is NACK'd and fails.
+  // For each chunk: try up to maxRetries times, waiting retryDelay ms between tries.
+  uint8_t maxTries = maxRetries;
+
+  while ((bytesRead < packetLength) && (maxTries > 0))
+  {
+    uint8_t bytesToRead; // Read the data in chunks of readWriteChunkSize max
+    if ((packetLength - bytesRead) > ((uint16_t)readWriteChunkSize))
+      bytesToRead = readWriteChunkSize;
+    else
+      bytesToRead = packetLength - bytesRead;
+
+    _i2cPort->beginTransmission(static_cast<int>(address));
+
+    uint8_t temp = static_cast<uint16_t>(registerAddress + bytesRead) >> 8;
+    _i2cPort->write(temp);
+    temp = (registerAddress + bytesRead) & 0xff;
+    _i2cPort->write(temp);
+
+    success = (_i2cPort->endTransmission() == 0);
+
+    if (success)
+    {
+      success = (_i2cPort->requestFrom(static_cast<int>(address), static_cast<int>(bytesToRead)) == bytesToRead);
+
+      if (success)
+      {
+        for (uint16_t i = 0; i < bytesToRead; i++)
+          buffer[i + bytesRead] = _i2cPort->read();
+
+        bytesRead += bytesToRead;
+        maxTries = maxRetries;
+      }
+    }
+
+    if (!success)
+    {
+      delay(retryDelay);
+      maxTries--;
+    }
+  }
 
   return success;
 }
 
-void SFE_ST2525DV64KC_IO::readMultipleBytes(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, uint8_t *const buffer, const uint16_t packetLength)
+bool SFE_ST2525DV64KC_IO::readSingleByte(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, uint8_t *value)
 {
-  _i2cPort->beginTransmission(static_cast<int>(address));
+  // If the IC is busy - e.g. completing a previous write - the I2C transmission is NACK'd and fails.
+  // Try up to maxRetries times, waiting retryDelay ms between tries.
+  uint8_t maxTries = maxRetries;
+  bool success = false;
 
-  uint8_t temp = static_cast<uint16_t>(registerAddress) >> 8;
-  _i2cPort->write(temp);
-  temp = registerAddress & 0xff;
-  _i2cPort->write(temp);
+  while ((maxTries > 0) && (!success))
+  {
+    _i2cPort->beginTransmission(static_cast<int>(address));
 
-  _i2cPort->endTransmission();
+    uint8_t temp = static_cast<uint16_t>(registerAddress) >> 8;
+    _i2cPort->write(temp);
+    temp = (registerAddress) & 0xff;
+    _i2cPort->write(temp);
 
-  _i2cPort->requestFrom(static_cast<int>(address), static_cast<int>(packetLength));
-  for (uint16_t i = 0; (i < packetLength) && _i2cPort->available(); i++)
-    buffer[i] = _i2cPort->read();
-}
+    success = (_i2cPort->endTransmission() == 0);
 
-uint8_t SFE_ST2525DV64KC_IO::readSingleByte(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress)
-{
-  uint8_t result;
-  _i2cPort->beginTransmission(static_cast<int>(address));
+    if (success)
+    {
+      success = (_i2cPort->requestFrom(static_cast<int>(address), 1U) == 1U);
 
-  uint8_t temp = static_cast<uint16_t>(registerAddress) >> 8;
-  _i2cPort->write(temp);
-  temp = registerAddress & 0xff;
-  _i2cPort->write(temp);
-  _i2cPort->endTransmission();
+      if (success)
+        *value = _i2cPort->read();
+    }
 
-  _i2cPort->requestFrom(static_cast<int>(address), 1U);
-  result = _i2cPort->read();
-  return result;
+    if (!success)
+    {
+      delay(retryDelay);
+      maxTries--;
+    }
+  }
+
+  return success;
 }
 
 bool SFE_ST2525DV64KC_IO::writeSingleByte(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, const uint8_t value)
 {
-  _i2cPort->beginTransmission(static_cast<int>(address));
+  // If the IC is busy - e.g. completing a previous write - the I2C transmission is NACK'd and fails.
+  // Try up to maxRetries times, waiting retryDelay ms between tries.
+  uint8_t maxTries = maxRetries;
+  bool success = false;
 
-  uint8_t temp = static_cast<uint16_t>(registerAddress) >> 8;
-  _i2cPort->write(temp);
-  temp = registerAddress & 0xff;
-  _i2cPort->write(temp);
+  while ((maxTries > 0) && (!success))
+  {
+    _i2cPort->beginTransmission(static_cast<int>(address));
 
-  _i2cPort->write(value);
+    uint8_t temp = static_cast<uint16_t>(registerAddress) >> 8;
+    _i2cPort->write(temp);
+    temp = registerAddress & 0xff;
+    _i2cPort->write(temp);
 
-  bool success = (_i2cPort->endTransmission() == 0);
+    _i2cPort->write(value);
 
-  // IC needs approx. 5ms to complete its writing phase.
-  delay(10);
+    success = (_i2cPort->endTransmission() == 0);
+
+    if (!success)
+    {
+      delay(retryDelay);
+      maxTries--;
+    }
+  }
 
   return success;
 }
 
-void SFE_ST2525DV64KC_IO::setRegisterBit(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, const uint8_t bitMask)
+bool SFE_ST2525DV64KC_IO::setRegisterBit(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, const uint8_t bitMask)
 {
-  uint8_t value = readSingleByte(address, registerAddress);
+  uint8_t value;
+  
+  if (!readSingleByte(address, registerAddress, &value))
+    return false;
+
   value |= bitMask;
-  writeSingleByte(address, registerAddress, value);
+
+  return writeSingleByte(address, registerAddress, value);
 }
 
-void SFE_ST2525DV64KC_IO::clearRegisterBit(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, const uint8_t bitMask)
+bool SFE_ST2525DV64KC_IO::clearRegisterBit(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, const uint8_t bitMask)
 {
-  uint8_t value = readSingleByte(address, registerAddress);
+  uint8_t value;
+  
+  if (!readSingleByte(address, registerAddress, &value))
+    return false;
+
   value &= ~bitMask;
-  writeSingleByte(address, registerAddress, value);
+
+  return writeSingleByte(address, registerAddress, value);
 }
 
 bool SFE_ST2525DV64KC_IO::isBitSet(const SF_ST25DV64KC_ADDRESS address, const uint16_t registerAddress, const uint8_t bitMask)
 {
-  uint8_t value = readSingleByte(address, registerAddress);
+  uint8_t value;
+  
+  if (!readSingleByte(address, registerAddress, &value))
+    return false;
+
   return (value & bitMask);
 }
